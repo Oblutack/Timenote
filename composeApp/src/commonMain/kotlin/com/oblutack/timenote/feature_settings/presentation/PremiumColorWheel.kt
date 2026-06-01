bbc88f3f08ead509c0daf0ac464a2a2fc1340334
Oblutack <kamenjas.evvel@gmail.com>
2026-06-01 21:58:21 +0200

    fix: align color wheel sweep gradient with HSV math

--- composeApp/src/commonMain/kotlin/com/oblutack/timenote/feature_settings/presentation/PremiumColorWheel.kt
+++ composeApp/src/commonMain/kotlin/com/oblutack/timenote/feature_settings/presentation/PremiumColorWheel.kt
@@ -49,8 +49,8 @@ fun PremiumColorWheel(
         drawCircle(
             brush = Brush.sweepGradient(
                 colors = listOf(
-                    Color.Red, Color.Magenta, Color.Blue, Color.Cyan,
-                    Color.Green, Color.Yellow, Color.Red
+                    Color.Red, Color.Yellow, Color.Green, Color.Cyan,
+                    Color.Blue, Color.Magenta, Color.Red
                 ),
                 center = center
             ),
